fe871dbf6bd449079f6105aa14f982d0692397fb
geovana280706-sketch <258940373+geovana280706-sketch@users.noreply.github.com>
2026-06-28 23:16:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Criação da função
+function exibirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Chamada da função 3 vezes
+exibirMensagem();
+exibirMensagem();
+exibirMensagem();
